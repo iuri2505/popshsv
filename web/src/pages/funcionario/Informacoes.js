d2f1cd4ac6a0e8f44a2feb794c51527b73eb7f4c
Iuri <iurisantos892@gmail.com>
2023-11-17 14:15:19 -0300

    commit 4

--- web/src/pages/funcionario/Informacoes.js
+++ web/src/pages/funcionario/Informacoes.js
@@ -1,7 +1,8 @@
-import { Grid, Typography } from '@mui/material';
+import { Grid, Typography, Container, Paper } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
+import TopBar from '../../components/TopBar';
 
 const Informacoes = () => {
 	const { id } = useParams();
@@ -28,7 +29,6 @@ const Informacoes = () => {
 
 	useEffect(() => {
 		if (funcionario.idSetorFK) {
-			console.log('never back down never what?');
 			const getSetor = async () => {
 				const response = await axios.get(
 					`http://localhost:3001/setor/${funcionario.idSetorFK}`
@@ -41,134 +41,147 @@ const Informacoes = () => {
 	}, [funcionario.idSetorFK]);
 
 	return (
-		<div
-			style={{
-				display: 'flex',
-				justifyContent: 'center',
-				alignItems: 'center',
-				height: '100vh',
-			}}
-		>
-			<Grid
-				container
-				spacing={2}
-			>
-				<Grid
-					item
-					xs={3}
-				>
-					<Typography variant='h6'>ID:</Typography>
-					<Typography variant='body2'>{funcionario.id}</Typography>
-				</Grid>
-				<Grid
-					item
-					xs={3}
-				>
-					<Typography variant='h6'>Nome:</Typography>
-					<Typography variant='body2'>{funcionario.nome}</Typography>
-				</Grid>
-				<Grid
-					item
-					xs={3}
-				>
-					<Typography variant='h6'>CPF:</Typography>
-					<Typography variant='body2'>{funcionario.cpf}</Typography>
-				</Grid>
-				<Grid
-					item
-					xs={3}
-				>
-					<Typography variant='h6'>CNPJ:</Typography>
-					<Typography variant='body2'>{funcionario.cnpj}</Typography>
-				</Grid>
-				<Grid
-					item
-					xs={3}
-				>
-					<Typography variant='h6'>Conselho:</Typography>
-					<Typography variant='body2'>
-						{funcionario.conselho}
-					</Typography>
-				</Grid>
-				<Grid
-					item
-					xs={3}
-				>
-					<Typography variant='h6'>Número Conselho:</Typography>
-					<Typography variant='body2'>
-						{funcionario.numeroConselho}
-					</Typography>
-				</Grid>
-				<Grid
-					item
-					xs={3}
-				>
-					<Typography variant='h6'>Especialidade:</Typography>
-					<Typography variant='body2'>
-						{funcionario.especialidade}
-					</Typography>
-				</Grid>
-				<Grid
-					item
-					xs={3}
-				>
-					<Typography variant='h6'>Classificação:</Typography>
-					<Typography variant='body2'>
-						{funcionario.classificacao}
-					</Typography>
-				</Grid>
-				<Grid
-					item
-					xs={3}
-				>
-					<Typography variant='h6'>CNS:</Typography>
-					<Typography variant='body2'>{funcionario.cns}</Typography>
-				</Grid>
-				<Grid
-					item
-					xs={3}
-				>
-					<Typography variant='h6'>Faturamento:</Typography>
-					<Typography variant='body2'>
-						{funcionario.faturamento}
-					</Typography>
-				</Grid>
-				<Grid
-					item
-					xs={3}
-				>
-					<Typography variant='h6'>Convênio:</Typography>
-					<Typography variant='body2'>
-						{funcionario.convenio}
-					</Typography>
-				</Grid>
-				<Grid
-					item
-					xs={3}
-				>
-					<Typography variant='h6'>Atuação:</Typography>
-					<Typography variant='body2'>
-						{funcionario.atuacao}
-					</Typography>
-				</Grid>
-				<Grid
-					item
-					xs={10}
-				>
-					<Typography variant='h6'>Observação:</Typography>
-					<Typography variant='body2'>
-						{funcionario.observacao}
-					</Typography>
-				</Grid>
-				<Grid
-					item
-					xs={3}
-				>
-					<Typography variant='h6'>Setor:</Typography>
-					<Typography variant='body2'>{setor.nome}</Typography>
-				</Grid>
-			</Grid>
-		</div>
+		<>
+			<TopBar />
+			<Container>
+				<Typography variant='h4'>Informações de {setor}</Typography>
+				<Container component={Paper}>
+					<Grid
+						container
+						spacing={2}
+					>
+						<Grid
+							item
+							xs={3}
+						>
+							<Typography variant='h6'>ID:</Typography>
+							<Typography variant='body2'>
+								{funcionario.id}
+							</Typography>
+						</Grid>
+						<Grid
+							item
+							xs={3}
+						>
+							<Typography variant='h6'>Nome:</Typography>
+							<Typography variant='body2'>
+								{funcionario.nome}
+							</Typography>
+						</Grid>
+						<Grid
+							item
+							xs={3}
+						>
+							<Typography variant='h6'>CPF:</Typography>
+							<Typography variant='body2'>
+								{funcionario.cpf}
+							</Typography>
+						</Grid>
+						<Grid
+							item
+							xs={3}
+						>
+							<Typography variant='h6'>CNPJ:</Typography>
+							<Typography variant='body2'>
+								{funcionario.cnpj}
+							</Typography>
+						</Grid>
+						<Grid
+							item
+							xs={3}
+						>
+							<Typography variant='h6'>Conselho:</Typography>
+							<Typography variant='body2'>
+								{funcionario.conselho}
+							</Typography>
+						</Grid>
+						<Grid
+							item
+							xs={3}
+						>
+							<Typography variant='h6'>
+								Número Conselho:
+							</Typography>
+							<Typography variant='body2'>
+								{funcionario.numeroConselho}
+							</Typography>
+						</Grid>
+						<Grid
+							item
+							xs={3}
+						>
+							<Typography variant='h6'>Especialidade:</Typography>
+							<Typography variant='body2'>
+								{funcionario.especialidade}
+							</Typography>
+						</Grid>
+						<Grid
+							item
+							xs={3}
+						>
+							<Typography variant='h6'>Classificação:</Typography>
+							<Typography variant='body2'>
+								{funcionario.classificacao}
+							</Typography>
+						</Grid>
+						<Grid
+							item
+							xs={3}
+						>
+							<Typography variant='h6'>CNS:</Typography>
+							<Typography variant='body2'>
+								{funcionario.cns}
+							</Typography>
+						</Grid>
+						<Grid
+							item
+							xs={3}
+						>
+							<Typography variant='h6'>Faturamento:</Typography>
+							<Typography variant='body2'>
+								{funcionario.faturamento}
+							</Typography>
+						</Grid>
+						<Grid
+							item
+							xs={3}
+						>
+							<Typography variant='h6'>Convênio:</Typography>
+							<Typography variant='body2'>
+								{funcionario.convenio}
+							</Typography>
+						</Grid>
+						<Grid
+							item
+							xs={3}
+						>
+							<Typography variant='h6'>Atuação:</Typography>
+							<Typography variant='body2'>
+								{funcionario.atuacao}
+							</Typography>
+						</Grid>
+						<Grid
+							item
+							xs={10}
+						>
+							<Typography variant='h6'>Observação:</Typography>
+							<Typography variant='body2'>
+								{funcionario.observacao}
+							</Typography>
+						</Grid>
+						<Grid
+							item
+							xs={3}
+						>
+							<Typography variant='h6'>Setor:</Typography>
+							<Typography variant='body2'>
+								{setor.nome}
+							</Typography>
+						</Grid>
+					</Grid>
+				</Container>
+			</Container>
+		</>
 	);
 };
 
